24290535df3c03e72cd56f24c4a891c8b182a063
Zachary Ouellet <zacharyouellet@outlook.com>
2026-08-04 19:04:23 -0400

    remove dock plugin for a ressource based implementation

--- project/addons/dtw_keyword_spotting/dtw_keyword_spotting.gd
+++ project/addons/dtw_keyword_spotting/dtw_keyword_spotting.gd
@@ -2,7 +2,6 @@
 class_name DtwKeywordSpotting
 extends EditorPlugin
 
-var dock
 static var open_datasets: Array[KeywordDataset] = []
 
 const InspectorPluginScript := preload("res://addons/dtw_keyword_spotting/keyword_dataset_inspector_plugin.gd")
@@ -10,7 +9,7 @@ var inspector_plugin: EditorInspectorPlugin
 
 func _save_external_data() -> void:
 	for dataset in open_datasets:
-		if is_instance_valid(dataset) and dataset._dirty:
+		if is_instance_valid(dataset): # TODO
 			print("Baking dataset : ", dataset)
 			dataset.bake_features()
 
@@ -25,22 +24,9 @@ func _disable_plugin() -> void:
 
 
 func _enter_tree() -> void:
-	dock = EditorDock.new()
-	dock.title = "Keyword library maker"
-	dock.default_slot = EditorDock.DOCK_SLOT_LEFT_UR
-	var dock_content = preload("res://addons/dtw_keyword_spotting/dock.tscn").instantiate()
-	dock.add_child(dock_content)
-	add_dock(dock)
-
 	inspector_plugin = InspectorPluginScript.new()
 	add_inspector_plugin(inspector_plugin)
 
 func _exit_tree() -> void:
-	remove_dock(dock)
-	dock.queue_free()
-	dock = null
-
 	remove_inspector_plugin(inspector_plugin)
 	inspector_plugin = null
-
-
--- project/addons/dtw_keyword_spotting/resource/wavList.gd
+++
@@ -1,5 +0,0 @@
-@tool
-class_name WavList
-extends Resource
-
-@export var wavs: Array[AudioStreamWAV]
